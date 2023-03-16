a09f8c57f48eefd1cf93ec776f76c24644ddba7a
ByulKim <byulkim1225@gmail.com>
2023-03-16 22:18:55 +0900

    Page Query Study

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,15 +1,30 @@
+import { graphql, PageProps } from 'gatsby';
 import React from 'react';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
 
 
-export default function blog() {
+export default function blog({data}: PageProps<Queries.BlogTitleQuery>) {
+    console.log(data)
     return (
         <Layout title="Blog">
             <p>blog text</p>
+            <ul>
+                {data.allFile.nodes.map((file, index) => <li key={index}>{file.name}</li>)}
+            </ul>
         </Layout>
     );
 }
+
+export const query = graphql`
+    query BlogTitle {
+    allFile {
+            nodes {
+                name
+            }
+        }
+    }
+`
 
 export const Head = ()=> <Seo title="Blog"/>
 
