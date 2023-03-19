315a41bf45a5bfbdb78c88f8de573c2f8282f4c3
ByulKim <byulkim1225@gmail.com>
2023-03-19 21:02:06 +0900

    dynamic image study

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,4 +1,5 @@
 import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import React from "react";
 import Layout from "../../components/Layout";
 import Seo from "../../components/Seo";
@@ -9,12 +10,17 @@ interface IBlogPostProps {
 }
 
 export default function BlogPost({data, children}: IBlogPostProps) {
-    console.log(data)
-    console.log(children)
+    console.log(data);
+    console.log(children);
+    const image = getImage(data.mdx?.frontmatter?.headerImage?.childImageSharp?.gatsbyImageData!);
     return (
         <Layout 
             title=""
         >
+            <GatsbyImage 
+                image={image as any}
+                alt={data.mdx?.frontmatter?.title!}
+            />
             <div>{children}</div>
         </Layout>
     )
@@ -29,7 +35,12 @@ export const query = graphql`
             category
             date
             slug
-        title
+            title
+            headerImage {
+                childImageSharp {
+                    gatsbyImageData(height: 450, placeholder: BLURRED)
+                }
+            }
         }
     }
     }
