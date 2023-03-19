5b2415c0f146161075d0a8962fff282ca502a77e
ByulKim <byulkim1225@gmail.com>
2023-03-19 22:51:22 +0900

    contentful study

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,3 +1,4 @@
+require("dotenv").config();
 import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
@@ -6,11 +7,15 @@ const config: GatsbyConfig = {
     siteUrl: `https://www.yourdomain.tld`,
     description: "Gatsby Study!!!",
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `y7vuq0by0w4f`,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     `gatsby-plugin-mdx`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import * as React from "react"
 import Layout from "../components/Layout"
 import Seo from "../components/Seo"
-import { StaticImage } from "gatsby-plugin-image"
+import { GatsbyImage, getImage, StaticImage } from "gatsby-plugin-image"
+import { graphql, PageProps } from "gatsby"
 
-export default function IndexPage() {
+export default function IndexPage({ data }:PageProps<Queries.ProductsQuery>) {
   return (
     <Layout title="Welcom to Dev Stickers">
       <div>
@@ -11,10 +12,40 @@ export default function IndexPage() {
         <StaticImage src="https://images.unsplash.com/photo-1625768376503-68d2495d78c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80" alt="Stickers"
         />
       </div>
+      <div>
+        {
+          data.allContentfulProduct.nodes.map((item, index) => {
+            return(
+              <div key={index}>
+                <GatsbyImage
+                  image={getImage(item.preview?.gatsbyImageData!)!}
+                  alt={item.name!}
+                />
+                <h2>{item.name}</h2>
+                <h4>$ {item.price}</h4>
+              </div>
+            )
+          })
+        }
+      </div>
     </Layout>
     
   )
 }
+export const query = graphql`
+  query Products {
+    allContentfulProduct {
+      nodes {
+        name
+        price
+        preview {
+          gatsbyImageData(placeholder: BLURRED, height: 250)
+        }
+      }
+    }
+  }
+`
+
 
 export const Head = ()=> <Seo title="Home"/>
 
